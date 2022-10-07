60398275da959ac1580bdacd2ac2025b56ecbe12
saber <ahmed.saber@msa.com>
2022-10-07 17:13:26 +0200

    v0.0 empty file

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main(void)
+{
+int x,y,z;
+printf("Enter x\n");
+scanf("%d",&x);
+printf("Enter y\n");
+scanf("%d",&y);
+z=x+y;
+printf("the result %d",z);
+
+
+}
